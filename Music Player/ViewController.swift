19f9fc4fe9caa10a59b9063d6308dc7b6623ed7e
vladun13 <nemyrovsky1311vlad@gmail.com>
2019-08-28 13:17:12 +0300

    Initial Commit

---
+++ Music Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music Player
+//
+//  Created by me on 28/08/2019.
+//  Copyright © 2019 Vlad Nemyrovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
